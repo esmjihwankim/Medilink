3c524075690d1c709d91da5ad54a78b8c7902ea4
rlaxodnd98 <rlaxodnd97@gmail.com>
2021-05-12 10:25:36 +0900

    2nd Robot arm --version 1.1

--- html/webapp/webapp.py
+++ html/webapp/webapp.py
@@ -16,32 +16,37 @@ from adafruit_motor import servo
 Initial Setup
 """
 app = Flask(__name__)
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(12, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(12, GPIO.OUT)   # LED control pin
 
-# servo = GPIO.PWM(12,50)
-# servo.start(0)
 i2c = busio.I2C(SCL, SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
+servo1 = servo.Servo(pca.channels[1])
+servo2 = servo.Servo(pca.channels[2])
+servo3 = servo.Servo(pca.channels[3], min_pulse=100, max_pulse=2600)
+servo4 = servo.Servo(pca.channels[4])
+servo5 = servo.Servo(pca.channels[5])
+servo6 = servo.Servo(pca.channels[6])
+servo7 = servo.Servo(pca.channels[7])
+"""
+initial angle
+"""
+servo1.angle = 40
+servo2.angle = 120
+servo3.angle = 60
+servo4.angle = 90
+servo5.angle = 90
+servo6.angle = 120
+servo7.angle = 50
+time.sleep(0.05)
 
 """
 APP.ROUTE
 """
-
 class S(object):
     _instance = None
 
-    # SERVO_MAX_DUTY=12
-    # SERVO_MIN_DUTY=3
-    # motor_angle=0
-    servo1 = servo.Servo(pca.channel[1])
-    servo2 = servo.Servo(pca.channel[2])
-    servo3 = servo.Servo(pca.channel[3], min_pulse=100, max_pulse=2600)
-    servo1.angle = 40
-    servo2.angle = 120
-    servo3.angle = 60
-    time.sleep(0.05)
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls._instance = super(Singleton, cls).__new__(cls)
@@ -49,17 +54,38 @@ class S(object):
 
 def move_left_right(x):
     angle3 = servo3.angle + x
+    angle7 = servo7.angle + x
     if angle3 < 90 and angle3 > 35:
         servo3.angle = angle3
-        time.sleep(0.05)
+    if angle7 < 80 and angle7 > 25:
+        servo7.angle = angle7
+    time.sleep(0.05)
 
 def move_up_down(y):
     angle1 = servo1.angle + y
     angle2 = servo2.angle - y
+    if servo5.angle > 60:
+        angle4 = servo4.angle
+        angle5 = servo5.angle - y
+        angle6 = servo6.angle - y
+    else :
+    #elif servo5.angle > 30:
+        angle4 = servo4.angle
+        angle5 = servo5.angle - 1.5 * y
+        angle6 = servo6.angle - y
+    #else :
+    #    angle4 = servo4.angle + y
+    #    angle5 = servo5.angle
+    #    angle6 = servo6.angle
+    
     if angle1 < 120 and angle1 > 30:
         servo1.angle = angle1
     if angle2 < 125 and angle2 > 35:
         servo2.angle = angle2
+    if angle5 < 95 and angle5 > 5:
+        servo5.angle = angle5
+    if angle6 < 125 and angle6 > 55:
+        servo6.angle = angle6
     time.sleep(0.05)
     
 @app.route('/')
